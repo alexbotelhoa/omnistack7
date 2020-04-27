86c8c863fe91aacf487e9179d8cdaa2ca0560c7d
Alex Botelho <alexbotelho1@hotmail.com>
2020-04-27 18:21:13 -0300

    Backend Finalizado

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+ const server = require('http').Server(app)
+ const io = require('socket.io')(server)
+
+mongoose.connect('mongodb+srv://admin:200679@cluster0-bbus6.mongodb.net/test?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+})
+
+app.use((req, res, next) => {
+   req.io =io
+   next()
+})
+app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+app.use(require('./routes'))
+server.listen(3333)
