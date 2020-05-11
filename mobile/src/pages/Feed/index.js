7839712b50180053e5de9ff58fd8858e1c1a7bde
Alex Botelho <alexbotelho1@hotmail.com>
2020-05-11 13:44:38 -0300

    Mobile 50% pronto

--- mobile/src/pages/Feed/index.js
+++ mobile/src/pages/Feed/index.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api'
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
 
 import Styles from './styles';
+
+import more from '../../assets/more.png'
+import like from '../../assets/like.png'
+import comment from '../../assets/comment.png'
+import send from '../../assets/send.png'
 
 export default Feed = () => {
 
@@ -22,12 +27,43 @@ export default Feed = () => {
   }, []);
 
   return (
-    <View>
+    <View style={Styles.container}>
       <FlatList
         data={feeds}
         keyExtractor={feed => String(feed._id)}
         renderItem={({ item: feed }) => (
-          <Text>{feed.author}</Text>
+          <View style={Styles.feedItem}>
+            <View style={Styles.feedItemHeader}>
+              <View style={Styles.userInfo}>
+                <Text style={Styles.name}>{feed.author}</Text>
+                <Text style={Styles.place}>{feed.place}</Text>
+              </View>
+
+              <Image source={more} />
+            </View>
+
+            <Image style={Styles.feedImage} source={{ uri: `http://10.0.2.2:3333/${feed.image}` }} />
+
+            <View style={Styles.feedItemFooter}>
+              <View style={Styles.actions}>
+                <TouchableOpacity
+                  onPress={() => {}}
+                >
+                  <Image source={like} />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {}}
+                >
+                  <Image source={comment} />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {}}
+                >
+                  <Image source={send} />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
         )}
       />
     </View>
--- mobile/src/pages/Feed/styles.js
+++ mobile/src/pages/Feed/styles.js
@@ -3,5 +3,29 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
    container: {
       textAlign: 'center'
+   },
+   feedItem: {
+
+   },
+   feedItemHeader: {
+
+   },
+   userInfo: {
+
+   },
+   name: {
+      
+   },
+   place: {
+      
+   },
+   feedImage: {
+
+   },
+   feedItemFooter: {
+
+   },
+   actions: {
+      
    }
 })
