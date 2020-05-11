22690d1a14eae2825f09d08d5c8354d25a6986aa
Alex Botelho <alexbotelho1@hotmail.com>
2020-05-11 12:08:50 -0300

    Mobile - Feed em construção

--- mobile/src/pages/Feed/index.js
+++ mobile/src/pages/Feed/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api'
+import { View, Text, FlatList } from 'react-native';
 
 import Styles from './styles';
 
 export default Feed = () => {
+
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function loadFeeds() {
+    if (loading) return;
+    setLoading(true)
+
+    const response = await api.get('posts')
+    setFeeds([ ...feeds, ...response.data ])
+  } 
+
+  useEffect(() => {
+    loadFeeds();
+  }, []);
+
   return (
     <View>
-      <Text style={Styles.container}>Feed</Text>
+      <FlatList
+        data={feeds}
+        keyExtractor={feed => String(feed._id)}
+        renderItem={({ item: feed }) => (
+          <Text>{feed.author}</Text>
+        )}
+      />
     </View>
-  );
+  )
 }
